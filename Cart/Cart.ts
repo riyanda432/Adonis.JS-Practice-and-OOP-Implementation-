b0dcbd1636eabe643b9df3287096ce4406da93c2
@riyanda.febri <riyanda.febri@sinbad.co.id>
2021-03-27 16:51:49 +0700

    Implement product interface into class

--- Cart/Cart.ts
+++ Cart/Cart.ts
@@ -3,7 +3,7 @@ interface Product {
     qty: number;
 }
 
-class Cart {
+class Cart implements Product{
     /** define properties for cart */
     name: string;
     qty: number; 
@@ -14,7 +14,7 @@ class Cart {
 
     }
 
-    addNewProduct(name:string, qty:number) {
+    addNewProduct(name:string, qty:number): Product[] {
         this.products.push({name, qty});
         return this.products; 
     }
@@ -23,9 +23,10 @@ class Cart {
         return this.products.map(el =>  el.name + " (" + el.qty + ")")
     }
 
-    deleteProduct(name: string) {
+    deleteProduct(name: string): Product[] {
         const errMsg = 'Failed to remove product !';
         this.products.map(el => el.name === name ? this.products.splice(el, 1) : errMsg )
+        return this.products
     }
 
 }
