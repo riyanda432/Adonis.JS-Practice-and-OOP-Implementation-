1dc16d9a666476d8ecb23d2e910423fa96a61d7b
@riyanda.febri <riyanda.febri@sinbad.co.id>
2021-03-28 14:43:39 +0700

    Initialize client use svelte.js

---
+++ BeratBadan/client/src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Nested from './Nested.svelte';
+
+	let count = 0;
+
+	function handleClick() {
+		count += 1
+	}
+</script>
+
+<style>
+	p {
+		color: purple;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>
+
+<p>Berat Badan </p>
+<button on:click={handleClick}> 	
+	Clicked {count} {count === 0 ? 'time' : 'times'}
+</button>
+<Nested/>
